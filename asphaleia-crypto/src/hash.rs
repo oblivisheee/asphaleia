7840122db2d258f614ca9976c3f6baeff0ac3901
oblivisheee <molniya213y@gmail.com>
2024-08-16 14:01:38 +0300

    New hash algorithm.
    Added new hash algorithm - Blake3.

--- asphaleia-crypto/src/hash.rs
+++ asphaleia-crypto/src/hash.rs
@@ -1,7 +1,6 @@
-use hex::{FromHex, ToHex};
+pub use hex::{FromHex, ToHex};
 use serde::{Deserialize, Serialize};
 use sha3::{Digest, Sha3_256};
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Ord, PartialOrd, Serialize, Deserialize)]
 pub struct Sha256(pub [u8; 32]);
 
@@ -41,6 +40,33 @@ impl FromHex for Sha256 {
     }
 }
 
+pub struct Blake3(blake3::Hash);
+
+impl Blake3 {
+    pub fn new(data: &[u8]) -> Self {
+        Self(blake3::hash(data))
+    }
+    pub fn as_bytes(&self) -> &[u8] {
+        self.0.as_bytes()
+    }
+    pub fn from_bytes(bytes: [u8; 32]) -> Self {
+        Self(blake3::Hash::from_bytes(bytes))
+    }
+    pub fn to_hex(&self) -> String {
+        self.0.to_hex().to_string()
+    }
+    pub fn from_hex(hex: &str) -> Self {
+        Self(blake3::Hash::from_hex(hex).unwrap())
+    }
+}
+impl ToHex for Blake3 {
+    fn encode_hex<T: std::iter::FromIterator<char>>(&self) -> T {
+        self.0.as_bytes().encode_hex()
+    }
+    fn encode_hex_upper<T: std::iter::FromIterator<char>>(&self) -> T {
+        self.0.as_bytes().encode_hex_upper()
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
