f96ec1ec868ad1e8c4527c1e2617cc5d67ebae8b
oblivisheee <molniya213y@gmail.com>
2024-08-16 14:03:19 +0300

    Improved structure.

--- asphaleia-crypto/src/lib.rs
+++ asphaleia-crypto/src/lib.rs
@@ -1,3 +1,19 @@
-pub mod aes;
+pub mod certificate;
+pub mod ed25519;
+pub mod encrypt;
 pub mod hash;
 pub mod keys;
+pub mod tls;
+pub mod zksnarks;
+
+pub use argon2;
+pub use certificate::*;
+pub use ed25519::*;
+pub use encrypt::*;
+pub use hash::*;
+pub use keys::*;
+pub use ring;
+pub use tls::*;
+
+#[cfg(feature = "kyber")]
+pub use pqc_kyber;
